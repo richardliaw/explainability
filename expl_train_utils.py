91a293d2c8092e4d0f1b362a7f8451f618a437aa
Richard Liaw <rliaw@berkeley.edu>
2018-08-16 10:29:03 -0700

    fixed kernel restart issue

--- expl_train_utils.py
+++ expl_train_utils.py
@@ -6,7 +6,7 @@ from lime import lime_image
 from matplotlib.collections import PatchCollection
 from shapely.geometry import mapping
 from shapely.geometry import Point
-from skimage.segmentation import mark_boundaries
+from skimage.segmentation import mark_boundaries, find_boundaries
 
 
 class SubPlotter:
@@ -65,44 +65,55 @@ class SubPlotter:
             i += 1
         return info
     
-    def plot_rand_segs(self, model, x_train_batch, shapely_polygons_batch, configs_batch, num_segs=3):
+    def plot_rand_segs(self, model, used_ids, catId_to_catName, 
+                       x_train_batch, y_train_batch, shapely_polygons_batch, configs_batch, num_segs=3):
         self.fig, self.axeslist = plt.subplots(ncols=3, nrows=num_segs, figsize=(8 * 3, 10 * num_segs))
-        rand_selection = np.random.randint(0, high=len(x_train_batch) - 1, size=3)
-        print(rand_selection)
+        rand_selection = np.random.randint(0, high=len(x_train_batch) - 1, size=num_segs)
         
         for i in rand_selection:
             explainer = lime_image.LimeImageExplainer()
             expl = explainer.explain_instance(x_train_batch[i], model.predict, top_labels=1, hide_color=0,
-                                              num_samples=100, timed=True, batch_size=128,
+                                              num_samples=100, timed=True, batch_size=128, num_features=5,
                                               use_bandits=self.use_bandits)
-            score = calcIoU(shapely_polygons_batch[i], explainer.segments, explainer.features_to_use)
-            print(i, score)
+            # score = calcIoU(shapely_polygons_batch[i], expl)
             
-            img, polygons, colors = configs_batch[i]
+            bbox_img, polygons, colors = configs_batch[i]
             
             # Display original image
             _sp = self.axeslist.ravel()[self.graph_count]
-            _sp.imshow(img / 2 + 0.5, cmap=plt.gray())
+            _sp.imshow(x_train_batch[i])
+            index = np.argmax(y_train_batch[i])
+            catId = used_ids[index]
+            catName = catId_to_catName[catId]
+            _sp.set_title("True Label: {}".format(catName))
             _sp.set_axis_off()
             self.graph_count += 1
-
+            
             # Display superpixel contrib
             temp, mask = expl.get_image_and_mask(expl.top_labels[0], positive_only=False, 
                                                  num_features=5, hide_rest=False)
             _sp = self.axeslist.ravel()[self.graph_count]
-            _sp.imshow(mark_boundaries(temp, mask) / 2 + 0.5, cmap=plt.gray())
+            c = mark_boundaries(temp, mask)
+            outline = find_boundaries(expl.segments)
+            c[outline] = (1, 1, 0)
+            _sp.imshow(c / 2 + 0.5, cmap=plt.gray())
+            index = np.argmax(model.predict(x_train_batch[i][np.newaxis,]))
+            catId = used_ids[index]
+            catName = catId_to_catName[catId]
+            _sp.set_title("Predicted: {}".format(catName))
             _sp.set_axis_off()
             self.graph_count += 1
             
             # Display ground truth object segmentation
             _sp = self.axeslist.ravel()[self.graph_count]
             _sp.imshow(bbox_img)
-            ax = _sp.gca()
-            ax.set_autoscale_on(False)
+            _sp.set_title("Image: {}".format(i))
+            # _sp.set_title("Image: {}, IoU: {}".format(i, score))
+            # _sp.set_axis_off()
             p = PatchCollection(polygons, facecolor=colors, linewidths=0, alpha=0.4)
-            ax.add_collection(p)
+            _sp.add_collection(p)
             p = PatchCollection(polygons, facecolor='none', edgecolors=colors, linewidths=2)
-            ax.add_collection(p)
+            _sp.add_collection(p)
             self.graph_count += 1
 
     
@@ -116,11 +127,11 @@ class SubPlotter:
         self.fig, self.axeslist = plt.subplots(ncols=3, nrows=len(info), figsize=(8 * 3, 10 * len(info)))
         
         for i, expl in info:
-            img, polygons, colors = configs_batch[i]
+            bbox_img, polygons, colors = configs_batch[i]
             
             # Display original image
             _sp = self.axeslist.ravel()[self.graph_count]
-            _sp.imshow(img / 2 + 0.5, cmap=plt.gray())
+            _sp.imshow(bbox_img / 2 + 0.5, cmap=plt.gray())
             _sp.set_axis_off()
             self.graph_count += 1
 
@@ -152,30 +163,31 @@ class SubPlotter:
         print("Ran for {} seconds!! ({} minutes)".format(round(diff, 3), round(diff / 60, 3)))
 
 
-def calcIoU(gt_pgon, pred_seg, seg_ids):
+def calcIoU(gt_pgon, expl):
     """Calculate IoU of gt_mask (shapely Polygon) + LIME seg
     
     Args:
         gt_pgon (shapely Polygon): mask of ground truth object segmentation
         pred_seg (np.array): segmentation algorithm output
-        seg_ids (array-like): ids of impactful segments
+        expl (LIME image explanation instance)
     
     Returns:
         iou (float): measure of accuracy of LIME segmentation to ground truth
     """
     pred_coords = []
-    for num in seg_ids:
-        shape = np.where(pred_seg == num)
+    weights = expl.local_exp[expl.top_labels[0]]
+    pos_segments = [x[0] for x in weights if x[1] > 0]
+    for ft in pos_segments:
+        shape = np.where(expl.segments == ft)
         # Not sure why transformation is required ...
-        pred_coords.extend(list(zip(shape[1], shape[0] * -1 + pred_seg.shape[1])))
-    
-    gt_coords = mapping(gt_pgon)['coordinates']
+        pred_coords.extend(list(zip(shape[1], shape[0] * -1 + expl.segments.shape[1])))
     
     coords_in_intersection = 0
     for coord in pred_coords:
         if gt_pgon.contains(Point(coord)):
             coords_in_intersection += 1
+    print(coords_in_intersection)
     
     num_coords_in_union = len(pred_coords) + len(gt_coords) - coords_in_intersection
     
-    return coords_in_intersection / num_coords_in_union   
+    return coords_in_intersection / num_coords_in_union 
